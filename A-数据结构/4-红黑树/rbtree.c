c109f8df90476861cfb02a05a6dab21f3c128720
isshe <isshe@foxmail.com>
2018-10-22 17:12:43 +0800

    20181022: 添加旋转部分代码

--- A-数据结构/4-红黑树/rbtree.c
+++ A-数据结构/4-红黑树/rbtree.c
@@ -1,12 +1,120 @@
 
+#include <assert.h>
+
 #include "rbtree.h"
+
+#define RBTREE_NIL &_sentinel /* all leafs are sentinels */
+static rbtree_node_t _sentinel = {RBTREE_NIL, RBTREE_NIL, NULL, RBTREE_BLACK};
 
 int is_red(rbtree_node_t *node)
 {
-    if (!node)
-    {
+    if (!node) {
         return RBTREE_BLACK;
     }
 
     return node->color;
+}
+
+rbtree_t *rbtree_create(rbtree_compare_func compare_func)
+{
+    rbtree_t *tree = NULL;
+
+    tree = (rbtree_t *)malloc(sizeof(rbtree_t));
+    if (!tree) {
+        return NULL;
+    }
+
+    tree->size = 0;
+    tree->root = RBTREE_NIL;
+    tree->compare_func = compare_func;
+
+    return tree;
+}
+
+
+/**
+ *     [P]                          [P]
+ *      |                            |
+ *     [N]                          [T]
+ *     / \         ->               / \
+ *    [1][T]                      [N] [3]
+ *       / \                      / \
+ *      [2][3]                   [1][2]
+ */
+void rbtree_left_rotate(rbtree_t *tree, rbtree_node_t *node)
+{
+    rbtree_node_t *temp = NULL;
+
+    assert(tree);
+    assert(node);
+
+    temp = node->right;
+
+    // 更新x的right，和y->left的parent
+    node->right = temp->left;
+    if (temp->left != RBTREE_NIL) {
+        temp->left->parent = node;
+    }
+
+    // 重新建立y的parent链接
+    // 1.更新temp的父节点
+    temp->parent = node->parent;
+
+    // 2.更新原来x的父节点的子节点，或者更新根节点
+    if (node->parent) { // 根节点的parent = NULL
+        if (node == node->parent->left) {
+            node->parent->left = temp;
+        } else {
+            node->parent->right = temp;
+        }
+    } else {
+        tree->root = temp;
+    }
+
+    //更新y的left和x的parent
+    temp->left = node;
+    node->parent = temp;
+}
+
+
+/**
+ *     [P]             [P]            [P]               [P]
+ *      |               |              |                 |
+ *     [N]   ->    [T] [N]     ->     [T] [N]    ->     [T]
+ *     / \         / \ / \            / \ / \           / \
+ *    [T][1]      [2][3][1]          [2][3] [1]       [2] [N]
+ *    / \                                                 / \
+ *   [2][3]                                              [3][1]
+ */
+void rbtree_right_rotate(rbtree_t *tree, rbtree_node_t *node)
+{
+    rbtree_node_t *temp = NULL;
+
+    assert(tree);
+    assert(node);
+
+    temp = node->left;
+
+    // 更新node->left
+    node->left = temp->right;
+    if (temp->right != RBTREE_NIL) {
+        temp->right->parent = node;
+    }
+
+    // 更新temp->parent
+    temp->parent = node->parent;
+
+    if(node->parent) {
+        if (node == node->parent->left) {
+            node->parent->left = temp;
+        } else {
+            node->parent->right = temp;
+        }
+    } else {
+        tree->root = temp;
+    }
+
+    // 更新temp->right
+    temp->right = node;
+    node->parent = temp;
 }
--- A-数据结构/4-红黑树/rbtree.h
+++ A-数据结构/4-红黑树/rbtree.h
@@ -2,26 +2,42 @@
 #ifndef RBTREE_H_
 #define RBTREE_H_
 
-#define RBTREE_RED    1
-#define RBTREE_BLACK  0
+#include <stdio.h>
+
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+
+#define container_of(PTR,TYPE,MEMBER)    ({  \
+    const typeof(((TYPE *)0)->MEMBER) *__mptr=(PTR);  \
+    (TYPE *) ((char *)__mptr - offsetof(TYPE,MEMBER)); })
+
+enum rbcolor_e 
+{ 
+    RBTREE_BLACK = 0, 
+    RBTREE_RED 
+}; 
+typedef enum rbcolor_e rbcolor_t;
 
 typedef struct rbtree_node_s  rbtree_node_t;
 struct rbtree_node_s {
     rbtree_node_t       *left;
     rbtree_node_t       *right;
     rbtree_node_t       *parent;
-    unsigned char       color;
+    rbcolor_t           color;
 };
 
 typedef int (*rbtree_compare_func) (rbtree_node_t *node1, rbtree_node_t *node2);
 
 typedef struct rbtree_s  rbtree_t;
 struct rbtree_s {
-    rbtree_node_t     *root;
-    rbtree_compare_func compare;
+    size_t size;                        // 记录树的大小
+    rbtree_node_t     *root;            // 树的根
+    rbtree_compare_func compare_func;   // 比较函数
 };
 
 
 int is_red(rbtree_node_t *node);
+
+rbtree_t *rbtree_create(rbtree_compare_func compare_func);
+
 
 #endif
