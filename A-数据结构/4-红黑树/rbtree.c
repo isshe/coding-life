fd337185e4578a6178d0a107863595e420dfbbe1
isshe <isshe@foxmail.com>
2018-10-24 18:10:37 +0800

    20181023: 添加红黑树插入代码

--- A-数据结构/4-红黑树/rbtree.c
+++ A-数据结构/4-红黑树/rbtree.c
@@ -15,7 +15,7 @@ int is_red(rbtree_node_t *node)
     return node->color;
 }
 
-rbtree_t *rbtree_create(rbtree_compare_func compare_func)
+rbtree_t *rbtree_create(rbtree_insert_func insert_func, rbtree_compare_func compare_func)
 {
     rbtree_t *tree = NULL;
 
@@ -27,10 +27,11 @@ rbtree_t *rbtree_create(rbtree_compare_func compare_func)
     tree->size = 0;
     tree->root = RBTREE_NIL;
     tree->compare_func = compare_func;
+    tree->insert_func = insert_func;
+    tree->same_flag = RBSAME_RETURN;
 
     return tree;
 }
-
 
 /**
  *     [P]                          [P]
@@ -117,4 +118,134 @@ void rbtree_right_rotate(rbtree_t *tree, rbtree_node_t *node)
     // 更新temp->right
     temp->right = node;
     node->parent = temp;
+}
+
+
+int temp_compare(rbtree_node_t *node1, rbtree_node_t *node2)
+{
+
+}
+
+int binary_tree_insert(rbtree_t *tree, rbtree_node_t *node, rbtree_node_t *sentinel)
+{
+    rbtree_node_t *temp, *parent;
+    int cmp = 0;
+
+    // 树空，直接插入后设为BLACK，返回
+    temp = tree->root;
+    parent = NULL;
+    while (temp != sentinel) {
+        cmp = tree->compare_func(temp, node);
+        parent = temp;
+        if (cmp == 0) {                         // 这里需要再考虑一下怎么处理，相同的值
+            //return RBTREE_ERROR;                // same value
+            switch(tree->same_flag)
+            {
+                case RBSAME_LEFT:
+                    temp = temp->left;
+                    break;
+                case RBSAME_RIGHT:
+                    temp = temp->right;
+                    break;
+                case RBSAME_RETURN:
+                default:
+                    return RBTREE_ERROR;
+            }
+        } else if (cmp < 0) {
+            temp = temp->left;
+        } else {
+            temp = temp->right;
+        }
+    }
+
+    node->left = sentinel;
+    node->right = sentinel;
+    node->parent = parent;
+
+    if (cmp < 0) {
+        parent->left = node;
+    } else {
+        parent->right = node;
+    }
+
+    return RBTREE_OK;
+}
+
+int rbtree_insert(rbtree_t *tree, rbtree_node_t *node)
+{
+    rbtree_node_t *temp, *sentinel;
+    int cmp = 0;
+    int rc = 0;
+
+    /* a binary tree insert */
+    sentinel = RBTREE_NIL;
+    if (tree->root == sentinel) {
+        node->parent = NULL;
+        node->left = sentinel;
+        node->right = sentinel;
+        rbnode_black(node);
+        tree->root = node;
+
+        return RBTREE_OK;
+    }
+
+    // 这里是自定义的插入，node 为 RED
+    rbnode_red(node);
+    rc = tree->insert_func(tree, node, sentinel);
+    if (rc != RBTREE_OK)
+    {
+        return rc;
+    }
+
+    /* re-balance tree */
+    // 当前节点node为RED，如果父节点也是RED，证明父节点不是根节点，那就至少还有3层
+    while (node != tree->root && rbnode_is_red(node->parent)) {
+
+        // 父节点是父父节点的左节点
+        if (node->parent == node->parent->parent->left) {
+            temp = node->parent->parent->right;
+
+            // 父节点及其兄弟节点都为红
+            if (rbnode_is_red(temp)) {                                  /*     [pp:b]                [pp:r](new n）*/
+                rbnode_black(node->parent);                             /*      /  \                  /  \         */ 
+                rbnode_black(temp);                                     /*   [p:r] [t:r]   (换色)   [p:b] [t:b]     */
+                rbnode_red(node->parent->parent);                       /*     |                     |             */
+                node = node->parent->parent;                            /*   [n:r]                 [n:r]           */
+
+            } else {                                                    /*     [pp:b]                [pp:b]        */
+                if (node == node->parent->right) {                      /*      /  \                  /  \         */
+                    node = node->parent;                                /*   [p:r] [t:b]   (左旋)   [p:r] [t:b]     */
+                    rbtree_left_rotate(tree, node);                     /*      \                   /              */
+                }                                                       /*     [n:r]              [n:r]            */
+
+                rbnode_black(node->parent);                             /*     [pp:b]                [p:b]         */
+                rbnode_red(node->parent->parent);                       /*      /  \                  /  \         */
+                rbtree_right_rotate(tree, node->parent->parent);        /*   [p:r] [t:b]   (右旋)   [n:r] [pp:r]    */
+            }                                                           /*    / \                           \      */
+                                                                        /*  [n:r]                          [t:b]   */
+        } else {
+            temp = node->parent->parent->left;
+
+            if (rbnode_is_red(temp)) {                                  /*     [pp:b]                [pp:r](new n）*/
+                rbnode_black(node->parent);                             /*      /  \                  /  \         */
+                rbnode_black(temp);                                     /*   [t:r] [p:r]   (换色)   [t:b] [p:b]     */
+                rbnode_red(node->parent->parent);                       /*           |                     |       */
+                node = node->parent->parent;                            /*         [n:r]                 [n:r]     */
+
+            } else {                                                    /*     [pp:b]                [pp:b]        */
+                if (node == node->parent->left) {                       /*      /  \                  /  \         */
+                    node = node->parent;                                /*   [t:b] [p:r]   (右旋)   [t:b] [p:r]     */
+                    rbtree_right_rotate(tree, node);                    /*          /                       \      */
+                }                                                       /*       [n:r]                     [n:r]   */
+
+                rbnode_black(node->parent);                             /*     [pp:b]                [p:b]         */
+                rbnode_red(node->parent->parent);                       /*      /  \                  /  \         */
+                rbtree_left_rotate(tree, node->parent->parent);         /*   [t:b] [p:r]   (左旋)   [pp:r] [n:r]    */
+            }                                                           /*           \              /              */
+        }                                                               /*          [n:r]        [t:b]             */
+    }
+
+    rbnode_black(tree->root);
+    rbtree_increace(tree);
+    return RBTREE_OK;
 }
--- A-数据结构/4-红黑树/rbtree.h
+++ A-数据结构/4-红黑树/rbtree.h
@@ -10,12 +10,29 @@
     const typeof(((TYPE *)0)->MEMBER) *__mptr=(PTR);  \
     (TYPE *) ((char *)__mptr - offsetof(TYPE,MEMBER)); })
 
+
+enum rbstatus_e
+{
+    RBTREE_OK = 0,
+    RBTREE_ERROR
+};
+typedef enum rbstatus_e rbstatus_t;
+
+#define rbtree_increace(tree)       ((tree)->size++)
+#define rbtree_decreace(tree)       ((tree)->size--)
+
 enum rbcolor_e 
 { 
     RBTREE_BLACK = 0, 
     RBTREE_RED 
-}; 
+};
 typedef enum rbcolor_e rbcolor_t;
+
+#define rbnode_red(node)               ((node)->color = RBTREE_RED)
+#define rbnode_black(node)             ((node)->color = RBTREE_RED)
+#define rbnode_is_red(node)            ((node)->color)
+#define rbnode_is_black(node)          (!ngx_rbt_is_red(node))
+#define rbnode_copy_color(n1, n2)      (n1->color = n2->color)
 
 typedef struct rbtree_node_s  rbtree_node_t;
 struct rbtree_node_s {
@@ -25,19 +42,30 @@ struct rbtree_node_s {
     rbcolor_t           color;
 };
 
+enum rbsame_e
+{
+    RBSAME_RETURN = 0,
+    RBSAME_LEFT ,
+    RBSAME_RIGHT
+};
+typedef enum rbsame_e rbsame_t;
+
 typedef int (*rbtree_compare_func) (rbtree_node_t *node1, rbtree_node_t *node2);
+typedef int (*rbtree_insert_func) (rbtree_t *tree, rbtree_node_t *node, rbtree_node_t *sentinel);
 
 typedef struct rbtree_s  rbtree_t;
 struct rbtree_s {
     size_t size;                        // 记录树的大小
     rbtree_node_t     *root;            // 树的根
     rbtree_compare_func compare_func;   // 比较函数
+    rbtree_insert_func insert_func;     // 插入函数
+    rbsame_t same_flag;
 };
 
 
 int is_red(rbtree_node_t *node);
 
-rbtree_t *rbtree_create(rbtree_compare_func compare_func);
+rbtree_t *rbtree_create(rbtree_insert_func insert_func, rbtree_compare_func compare_func);
 
 
 #endif
