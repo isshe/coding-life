5519e5e3f0d9cf7dd95efbc90f910dc10d1c5ec1
isshe <i.sshe@foxmail.com>
2019-11-06 20:08:40 +0800

    20191106: 添加读取pcap和解析ip位置的python脚本

--- N-安全/Python绝技：运用Python成为顶级黑客/4-用Python分析网络流量/print_geo.py
+++ N-安全/Python绝技：运用Python成为顶级黑客/4-用Python分析网络流量/print_geo.py
@@ -6,7 +6,12 @@ from geoip2 import database
 def print_record2(tgt):
     db = database.Reader("GeoLite2/GeoLite2-City.mmdb")
     rec = db.city(tgt)
+    
     print(rec)
+    print(rec.country)
+    print(rec.city)
+    print(rec.subdivisions)
+    print(rec.subdivisions[0].names["zh-CN"])
 
 
 tgt = "14.17.22.45"
--- N-安全/Python绝技：运用Python成为顶级黑客/4-用Python分析网络流量/print_pcap_geo.py
+++ N-安全/Python绝技：运用Python成为顶级黑客/4-用Python分析网络流量/print_pcap_geo.py
@@ -1,35 +1,56 @@
+# coding=utf-8
+
 import dpkt
 import socket
-import pygeoip
 import optparse
+from geoip2 import database
+
+global db
 
 def ret_geo_str(ip):
-    pass
-    
+    rec = db.city(ip)
+    return rec.subdivisions[0].names["zh-CN"]
+
 
 def print_pcap(pcap):
-    for (ta, buf) in pcap:
+    for (_, buf) in pcap:
         try:
             eth = dpkt.ethernet.Ethernet(buf)
-            ip = eth.data
+            ip = eth.data   # 这里是二进制数据
             src = socket.inet_ntoa(ip.src)
             dst = socket.inet_ntoa(ip.dst)
             print("[+] Src: " + src + " --> Dst: " + dst)
-            print("[+] Src: " + ret_geo_str(src) + " --> Dst: " + ret_geo_str(dst))
+            try:
+                src_city = ret_geo_str(src)
+            except:
+                src_city = "内网/未知"
+
+            try:
+                dst_city = ret_geo_str(dst)
+            except:
+                dst_city = "内网/未知"
+
+            if src_city and dst_city:
+                print("[-] 从: " + src_city + " --> 到: " + dst_city)
         except:
             pass
 
+
 def main():
-    parser = optparse.OptionParser("Usage%prog -p <pacp file>")
+    parser = optparse.OptionParser("Usage%prog -p <pacp_file.pcap> -g <geo_db_file.mmdb>")
     parser.add_option('-p', dest="pcap_file", type="string", help="specify pcap filename")
+    parser.add_option('-g', dest="geo_db", type="string", help="specify geo db filename")
 
     (options, _) = parser.parse_args()
-    if options.pcap_file is None:
+    if options.pcap_file is None or options.geo_db is None:
         print(parser.usage)
         exit(0)
+
+    db = database.Reader(options.geo_db)
+    
     pcap_file = options.pcap_file
-    f = open(pcap_file)
-    pcap = dpkt.pcap.Reader(f)  # ！！！
+    f = open(pcap_file, "rb") # 读取二进制文件，记得用rb
+    pcap = dpkt.pcap.Reader(f)
     print_pcap(pcap)
 
 if __name__ == "__main__":
