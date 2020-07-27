01ce52ebe5056ff1d3c30367a3d65a9ebca443ca
isshe <i.sshe@foxmail.com>
2020-07-28 00:08:41 +0800

    更新 redis 集群的 docker-compose.yml

--- D-工具/redis/python/3-sub-pub.py
+++ D-工具/redis/python/3-sub-pub.py
@@ -22,5 +22,24 @@ async def aioredis_pub_sub():
     redis.close()
     await redis.wait_closed()
 
+async def reader(channel):
+    async for ch, message in channel.iter():
+        print("Got message in channel:", ch, ":", message)
+
+async def aioredis_pub_sub_pattern():
+    r = await aioredis.create_redis_pool('redis://localhost:6379')
+    ch = await r.psubscribe('changel:*')
+    assert isinstance(ch, aioredis.Channel)
+
+    asyncio.get_running_loop().create_task(reader(ch))
+
+    await r.publish('channel:1', 'Hello')
+    await r.publish('channel:2', 'World')
+    r.close()
+    await r.wait_closed()
+
+
+
 if __name__ == "__main__":
     asyncio.run(aioredis_pub_sub())
+    asyncio.run(aioredis_pub_sub_pattern())
