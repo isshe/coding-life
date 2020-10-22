7272d87d5b0b27822a338652beb76cde59917cc7
isshe <shushen@openresty.com>
2020-10-21 21:38:17 -0700

    change: daily sync.

--- D-工具/Linux/scripts/sync-git.sh
+++ D-工具/Linux/scripts/sync-git.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 projects=(
-    /root/code/Personal/Notebook
-    # /root/code/Personal/coding-life
+    "/root/code/Personal/Notebook"
+    "/root/code/Personal/coding-life"
 )
 
-for project in $projects
+for project in ${projects[@]}
 do
+    echo "---- project: $project start -----"
+    cd $project
     no_changed=`git status | grep 'nothing to commit'`
-    if [ ! $no_changed ]; then
+    # echo "$no_changed"
+    if [[ "x$no_changed" == "x" ]]; then
         branch=`git branch | grep '* ' | awk -F ' ' '{print $2}'`
+        echo "[+] git pull: $branch"
         if [ $branch ]; then
             git pull origin $branch
         else
@@ -18,11 +22,14 @@ do
 
         git add .
         git commit -m "change: daily sync."
+
+        echo "[+] git push: $branch"
         if [ $branch ]; then
             git push origin $branch
         else
             git push
         fi
     fi
+    echo "---- project: $project finished -----"
 done
 
