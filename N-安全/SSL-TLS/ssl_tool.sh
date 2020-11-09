c6857ed44d1b6b0b1ac4b96a8c3e9c8e07b9c8ec
isshe <shushen@openresty.com>
2020-11-09 12:00:19 -0800

    change: daily sync.

--- N-安全/SSL-TLS/ssl_tool.sh
+++ N-安全/SSL-TLS/ssl_tool.sh
@@ -22,6 +22,17 @@ ca=${domain}-ca
 server=${domain}-server
 client=${domain}-client
 
+openssl genrsa -out ${ca}.key 2048
+
+openssl req -new -key ${ca}.key -out ${ca}.csr
+
+openssl x509 -req -days 365 -in ${ca}.csr -signkey ${ca}.key -out ${ca}.crt
+
+openssl genrsa -out ${client}.key 1024
+openssl req -new -out ${client}.csr -key ${client}.key
+openssl x509 -req -in ${client}.csr -out ${client}.crt -signkey ${client}.key -CA ${ca}.crt -CAkey ${ca}.key -CAcreateserial -days 3650
+openssl pkcs12 -export -clcerts -in ${client}.crt -inkey ${client}.key -out ${client}.p12
+
 openssl genrsa -out ${server}.key 1024
 
 openssl req -new -x509 -days ${day} -key ${server}.key -out ${server}.crt -subj "/C=CN/ST=Guangdong/L=Shenzhen/O=Global Security/OU=IT Department/CN=${domain}"
