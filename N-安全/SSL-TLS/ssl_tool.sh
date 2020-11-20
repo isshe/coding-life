c4c825f3018493e9fee7fe609a530b8b8fbdfa0a
isshe <shushen@openresty.com>
2020-11-20 12:00:16 -0800

    change: daily sync.

--- N-安全/SSL-TLS/ssl_tool.sh
+++ N-安全/SSL-TLS/ssl_tool.sh
@@ -3,12 +3,12 @@
 basepath=$(cd `dirname $0`; pwd)
 
 
-domain=www.edgetest.com
+domain=pkg.openresty.com
 if [ $1 ]; then
     domain=$1
 fi
 
-day=1
+day=3650
 if [ $2 ]; then
     day=$2
 fi
@@ -22,15 +22,25 @@ ca=${domain}-ca
 server=${domain}-server
 client=${domain}-client
 
+echo "[+] gen ca.key"
 openssl genrsa -out ${ca}.key 2048
 
-openssl req -new -key ${ca}.key -out ${ca}.csr
+echo "[+] gen ca.csr"
+openssl req -new -key ${ca}.key -out ${ca}.csr -subj "/C=CN/ST=Guangdong/L=Shenzhen/O=Global Security/OU=IT Department/CN=${domain}"
 
+echo "[+] gen ca.crt"
 openssl x509 -req -days 365 -in ${ca}.csr -signkey ${ca}.key -out ${ca}.crt
 
+echo "[+] gen client.key"
 openssl genrsa -out ${client}.key 1024
-openssl req -new -out ${client}.csr -key ${client}.key
+
+echo "[+] gen client.csr"
+openssl req -new -out ${client}.csr -key ${client}.key -subj "/C=CN/ST=Guangdong/L=Shenzhen/O=Global Security/OU=IT Department/CN=${domain}"
+
+echo "[+] gen client.crt"
 openssl x509 -req -in ${client}.csr -out ${client}.crt -signkey ${client}.key -CA ${ca}.crt -CAkey ${ca}.key -CAcreateserial -days 3650
+
+echo "[+] gen client.p12"
 openssl pkcs12 -export -clcerts -in ${client}.crt -inkey ${client}.key -out ${client}.p12
 
 openssl genrsa -out ${server}.key 1024
