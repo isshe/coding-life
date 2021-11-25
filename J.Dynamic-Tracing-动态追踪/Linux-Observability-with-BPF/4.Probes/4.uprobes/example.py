b19f7046fa940b826a2874b0333404db92589b60
Shushen He <shushen@openresty.com>
2021-11-25 06:00:04 -0400

    20211125: daily sync.

---
+++ J.Dynamic-Tracing-动态追踪/Linux-Observability-with-BPF/4.Probes/4.uprobes/example.py
@@ -0,0 +1,28 @@
+from bcc import BPF
+import os
+
+file_path = os.path.realpath(__file__)
+file_path = os.path.dirname(file_path)
+
+bpf_source = """
+int trace_main(struct pt_regs *ctx) {
+  u64 pid = bpf_get_current_pid_tgid();
+  bpf_trace_printk("New hello-bpf process running with PID: %d", pid);
+  return 0;
+}
+"""
+
+bpf = BPF(text=bpf_source)
+print("file_path = ", file_path)
+bpf.attach_uprobe(name="/tmp/hello-bpf-go.out",
+                  sym="main.main", fn_name="trace_main")
+bpf.trace_print()
+# me = os.getpid()
+# while 1:
+#     try:
+#         (task, pid, cpu, flags, ts, msg) = bpf.trace_fields()
+#     except ValueError:
+#         continue
+#     if pid == me or msg == "":
+#         continue
+#     print("%-18.9f %-16s %-6d %s" % (ts, task, pid, msg))
--- J.Dynamic-Tracing-动态追踪/Linux-Observability-with-BPF/4.Probes/README.md
+++ J.Dynamic-Tracing-动态追踪/Linux-Observability-with-BPF/4.Probes/README.md
@@ -11,6 +11,10 @@ Probes（探针）
 * TracePoints: These provide static access to internal components in the kernel.
     * 在 `/sys/kernel/debug/tracing/events` 可以看到系统中支持的 TracePoints。
 * User-Space Probes: These give you dynamic access to programs running in user-space.
+    * 也分为：
+        * uprobes：执行指令前的钩子
+        * uretprobes：执行指令后的钩子
+    * 可以用 `mn` 命令来检查程序中是否存在目标指令
 * User statically defined tracepoints: These allow static access to programs running in user-space.
 
 这里的“静态”和“非静态”是对于 ABI (application binary interface) 是否稳定来说的，静态就是稳定，在不同的内核版本中，也可通用。
