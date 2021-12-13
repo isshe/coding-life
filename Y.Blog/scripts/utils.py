bf964a289d8522f6f21faae2e150ab9bb1ccbf05
Shushen He <shushen@openresty.com>
2021-12-13 00:27:56 -0400

    change: update utils.py.

--- Y.Blog/scripts/utils.py
+++ Y.Blog/scripts/utils.py
@@ -3,9 +3,17 @@ import random
 
 
 def pick_color():
-    return "#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+    red = random.randint(80, 230)
+    green = random.randint(80, 230)
+    blue = random.randint(80, 230)
+    return '#%02x%02x%02x' % (red, green, blue)
+    # return "#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)])
 
 
 def pick_image(path):
     files = os.listdir(path)
     return random.choice(files)
+
+
+if __name__ == "__main__":
+    print(pick_color())
