25b51238efc3af02f504c978b44f2ba202de332a
robot <robot@openresty.com>
2023-10-24 06:00:02 +0800

    20231024: daily sync.

--- J.调试/动态追踪/SystemTap/最佳实践/003-c-stack-trace/1-run-and-trace.sh
+++ J.调试/动态追踪/SystemTap/最佳实践/003-c-stack-trace/1-run-and-trace.sh
@@ -16,6 +16,10 @@ stap -x $perl_pid \
     --no-global-var-display \
     --save-uprobes \
     --nonfatal-maxaction \
+    -d /usr/lib64/perl5/vendor_perl/auto/Time/HiRes/HiRes.so \
+    -d /usr/lib64/libz.so.1.2.11 \
+    -d /usr/lib64/libcrypt.so.2.0.0 \
+    -d /usr/lib64/ld-linux-x86-64.so.2 \
     -d /usr/lib64/libc.so.6 \
     -d /usr/lib64/libperl.so.5.32.1:/usr/lib/debug/.build-id/8b/9e38a8d738070b5cffea4a2fd9fde7c873d6a8.debug:/usr/lib/debug/.dwz/perl-5.32.1-480.el9.x86_64 \
     --print-modidx \
